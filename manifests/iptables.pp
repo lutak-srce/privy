b0959c81d24b2a305a8c9cbd9ef26b1af3c0c9f1
Zavisa Nikac <znikac@srce.hr>
2016-05-12 13:27:27 +0200

    syntax change

--- manifests/iptables.pp
+++ manifests/iptables.pp
@@ -1,19 +1,51 @@
 # Class: privy::iptables
 class privy::iptables (
   $source = 'puppet:///private/iptables',
-  ){
-#  include ::ulog
+  $source6 = 'puppet:///modules/srce/iptables/ip6tables-default',
+  ) {
 
-  file { '/etc/sysconfig/iptables':
-    ensure => file,
-    owner  => root,
-    group  => root,
-    mode   => '0600',
-    source => $source,
-  }
-  service { 'iptables':
-    ensure    => running,
-    enable    => true,
-    subscribe => File['/etc/sysconfig/iptables'],
+  case $facts['os']['family'] {
+    'RedHat' : {
+      if $facts['os']['release']['major'] == '7' { 
+        package { 'iptables-services': ensure => present, } 
+      }   
+      file { '/etc/sysconfig/iptables':
+        ensure => file,
+        owner  => root,
+        group  => root,
+        mode   => '0600',
+        source => $source,
+      }
+      service { 'iptables':
+        ensure    => running,
+        enable    => true,
+        subscribe => File['/etc/sysconfig/iptables'],
+      }
+    }
+    'Debian' : {
+      if $facts['os']['release']['major'] == '8' { 
+        package { 'iptables-persistent': ensure => present, }
+        file { '/etc/iptables/rules.v4':
+          ensure => file,
+          owner  => root,
+          group  => root,
+          mode   => '0644',
+          source => $source,
+        }
+        file { '/etc/iptables/rules.v6':
+          ensure => file,
+          owner  => root,
+          group  => root,
+          mode   => '0644',
+          source => $source6,
+        }
+        service { 'netfilter-persistent':
+          ensure    => running,
+          enable    => true,
+          subscribe => File['/etc/iptables/rules.v4','/etc/iptables/rules.v6'],
+        }
+      } else { notify{"privy::iptables does not support this OS !!!": } }
+    }
+    default: { notify{"privy::iptables does not support this OS !!!": } }
   }
 }
